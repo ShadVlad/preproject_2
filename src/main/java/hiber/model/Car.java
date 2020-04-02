8413cce4163b0f034d06975dd7a741406510ca9e
ShadVlad <wowchik.shadrin@yandex.ru>
2020-04-02 14:52:09 +0300

    Entity Car

---
+++ src/main/java/hiber/model/Car.java
@@ -0,0 +1,53 @@
+package hiber.model;
+
+import javax.persistence.*;
+
+@Entity(name = "car")
+@Table(name = "car")
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "series")
+    private String series;
+
+    @OneToOne(mappedBy = "car")
+    private User user;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSeries() {
+        return series;
+    }
+
+    public void setSeries(String series) {
+        this.series = series;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
